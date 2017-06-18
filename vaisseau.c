f3bd4f8cca8cfd9dcc5be27302cef54e37a8a897
Ahcamn <Ahcamn.ivalice@gmail.com>
2017-06-18 02:25:09 +0200

    Update vaisseau.c

--- vaisseau.c
+++ vaisseau.c
@@ -1,22 +1,5 @@
 #include "vaisseau.h"
 
-#define NB_DRONE_PETIT 6
-#define NB_DRONE_MOYEN 4
-#define NB_DRONE_GROS  2
-
-#define PETIT 1
-#define MOYEN 2
-#define GROS  3
-
-
-pthread_cond_t condition = PTHREAD_COND_INITIALIZER; /* Création de la condition */
-pthread_mutex_t mutex_drone = PTHREAD_MUTEX_INITIALIZER; /* Création du mutex */
-
-static int nbPetitColis;
-// static int nbMoyenColis;
-// static int nbGrosColis;
-
-
 void erreur(const char *msg)
 {
     perror(msg);
@@ -24,18 +7,25 @@ void erreur(const char *msg)
 
 
 int main()
-{
-    nbPetitColis = 10;
+{   
+    int i;
+    // int msgid;
+    srand(time(NULL));
     
-    pthread_t drone_petit[NB_DRONE_PETIT];    
-    pthread_t drone_moyen[NB_DRONE_MOYEN];    
-    pthread_t drone_gros[NB_DRONE_GROS];    
+    /*if ((msgid = msgget(CLE, IPC_CREAT | IPC_EXCL | 0600)) == -1)
+		erreur("Erreur creation file de messages");*/
     
-    createDroneThread(drone_petit, NB_DRONE_PETIT, PETIT);
-    createDroneThread(drone_moyen, NB_DRONE_MOYEN, MOYEN);
-    createDroneThread(drone_gros, NB_DRONE_GROS, GROS);
-        
-    printf("Création des Drones terminées !");
+    for(i=0; i< NB_CLIENTS; i++)
+        createClientThread(ville.client[i], i);
+    
+    createDroneThread(vaisseau.drone_petit, NB_DRONE_PETIT, PETIT);
+    // createDroneThread(vaisseau.drone_moyen, NB_DRONE_MOYEN, MOYEN);
+    // createDroneThread(vaisseau.drone_gros, NB_DRONE_GROS, GROS);
+    
+    
+    /*if(msgctl(msgid, IPC_RMID, 0) == -1)
+		erreur("Erreur suppresion de la file de messages\n");*/
+    
     exit(EXIT_SUCCESS);
 }
 
@@ -43,23 +33,32 @@ int main()
 void *fonc_droneP(void *arg) 
 {
     int type = (int)arg;
+    // int msgid;
     Drone d = createDrone(type);
+    Client c;
     
     while(1)
     {
-        pthread_mutex_lock(&mutex_drone);
+        pthread_mutex_lock(&vaisseau.mutex_drone);
         
-        if(nbPetitColis == 0 || d->autonomie <= 0)
+        if(vaisseau.nbPetitColis == 0 || d->autonomie <= 0)
         {
-            pthread_cond_wait(&condition, &mutex_drone);
+            pthread_cond_wait(&vaisseau.condition_drone, &vaisseau.mutex_drone);
         }
+        
+        /*if ((msgid = msgget(CLE, 0)) == -1) 
+            erreur("Ereur msgget Drone\n");
+        
+        if (msgrcv(msgid, &c, sizeof(Client), 1, 0) == -1)
+            perror("Erreur de lecture requete \n");*/
 
-        nbPetitColis--;
-        d->autonomie -= 1;
-        printf("Type : %d / Autonomie : %.1fh / Temps de recharge : %.1fh.\n", type, d->autonomie, d->tempsRecharge);
-        printf("Colis restant : %d\n", nbPetitColis);
-        pthread_mutex_unlock(&mutex_drone);
-    }
+        vaisseau.nbPetitColis--;
+        //d->autonomie -= c->tempsTrajet*2;
+		d->autonomie -= 40;
+        printf("ID : %lu / Type : %d / Autonomie : %.1fh / Temps de recharge : %.1fh.\n", pthread_self(), type, d->autonomie, d->tempsRecharge);
+        printf("Colis restant : %d\n", vaisseau.nbPetitColis);
+        pthread_mutex_unlock(&vaisseau.mutex_drone);
+    }   
           
     pthread_exit(NULL);
 } 
@@ -76,21 +75,21 @@ Drone createDrone(int type)
         switch(type)
         {
             case PETIT:
-                d->autonomie = 2;
-                d->tempsRecharge = 0.5f;
+                d->autonomie = 200;
+                d->tempsRecharge = 60.0f;
                 break;
             case MOYEN:
-                d->autonomie = 4;
-                d->tempsRecharge = 1.0f;
+                d->autonomie = 300;
+                d->tempsRecharge = 90.0f;
                 break;
             case GROS:
-                d->autonomie = 6;
-                d->tempsRecharge = 2.0f;
+                d->autonomie = 400;
+                d->tempsRecharge = 120.0f;
                 break;
         };
     }
     else
-        erreur("Erreur création Drone");
+        erreur("Erreur création Drone\n");
     
 	return d;
 }
@@ -98,13 +97,14 @@ Drone createDrone(int type)
 void createDroneThread(pthread_t *drone, int nbDrones, int type)
 {
     int i;
+    
     for(i=0; i<nbDrones; i++)
-    {
-        if(pthread_create(&drone[i], 0, fonc_droneP, (void*)type))
-            erreur("Erreur création thread");
-
+        if(pthread_create(&drone[i], NULL, fonc_droneP, (void*)type))
+            erreur("Erreur création thread Drone\n");
+    
+    for(i=0; i<nbDrones; i++)
         if(pthread_join(drone[i], NULL))
-            erreur("Erreur pthread_join");
-    }
+            erreur("Erreur pthread_join Drone\n");
+        
     printf("\n");
 }
