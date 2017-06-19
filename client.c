2d6fcaf6d08755dcc82b9b01966d60a818f76c27
Ahcamn <Ahcamn.ivalice@gmail.com>
2017-06-19 23:37:04 +0200

    Update client.c

--- client.c
+++ client.c
@@ -1,34 +1,6 @@
 #include "client.h"
 
-static Ville ville =
-{
-   .mutex_client = PTHREAD_MUTEX_INITIALIZER,
-   .condition_client = PTHREAD_COND_INITIALIZER,
-};
-
     
-void *fonc_client(void *arg) 
-{
-    int clientID = (int)arg + 1;
-    Client c = createClient(clientID);
-    
-    pthread_mutex_lock(&ville.mutex_client);
-    
-    if(c->couvert && c->jardin && c->present && c->tempsTrajet <= 30)
-    {
-       
-        pthread_cond_signal (&vaisseau.condition_drone);
-
-        printf("Client %d  => couvert : %d / jardin : %d / present : %d / tempsTrajet : %d minutes / colis : %d\n", clientID, c->couvert, c->jardin, c->present, c->tempsTrajet, c->colis);
-        
-    }
-        
-    pthread_mutex_unlock(&ville.mutex_client);
-    
-    pthread_exit(NULL);
-}
-
-
 Client createClient(int i)
 {
     Client c = malloc(sizeof(Client));
@@ -36,16 +8,16 @@ Client createClient(int i)
     if(c != NULL)
     {
         c->clientID = i;
-        c->couvert = pileOuface();
-        c->jardin = pileOuface();
-        c->present = pileOuface();
+        c->couvert = alea();
+        c->jardin = alea();
+        c->present = alea();
         c->tempsTrajet = rand()%21 + 10;
+        // c->tempsTrajet = 30;
         c->colis = rand()%3 + 1;
     }
     else
         erreur("Erreur création Drone\n");
     
-    printf("Client %d  => couvert : %d / jardin : %d / present : %d / tempsTrajet : %d minutes / colis : %d\n", c->clientID, c->couvert, c->jardin, c->present, c->tempsTrajet, c->colis);
     return c;
 }
 
@@ -53,12 +25,10 @@ void createClientThread(pthread_t client, int i)
 {
     if(pthread_create(&client, NULL, fonc_client, (void*)i))
         erreur("Erreur création thread Client\n");
-    
-    if(pthread_join(client, NULL))
-            erreur("Erreur pthread_join Client\n");
 }
 
-bool pileOuface()
+bool alea()
 {
-    return rand() % 2;
+    int r = rand() % 100 + 1;
+    return r <= 90;
 }
