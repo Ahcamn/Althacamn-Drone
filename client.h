a54a74345f68a4d5fc62c967e2b731c3f163fea9
Ahcamn <Ahcamn.ivalice@gmail.com>
2017-06-22 00:11:20 +0200

    Update client.h

--- client.h
+++ client.h
@@ -4,12 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/ipc.h> 
-#include <sys/sem.h> 
-#include <sys/msg.h>
 #include <pthread.h> 
 #include <stdbool.h>
+#include <time.h>
 
 #include "vaisseau.h"
 
